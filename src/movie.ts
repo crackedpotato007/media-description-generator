b170ae08e0725c547ac83642674be5198462120a
CrackedPotato <68459855+crackedpotato007@users.noreply.github.com>
2024-08-30 10:49:09 +0530

    Simply increase piece size to handle big files

--- src/movie.ts
+++ src/movie.ts
@@ -88,6 +88,6 @@ async function main(movie_name: string, path: string) {
     console.log(`[img]${screenshot}[/img]`);
   }
   // eslint-disable-next-line no-useless-escape
-  execSync(`mktorrent -a ${process.env.ANNOUNCE_URL!} -p \"${path}\" `);
+  execSync(`mktorrent -a ${process.env.ANNOUNCE_URL!} -p \"${path}\" -l 23`);
 }
 export default main;
